85a231cac7fbc8a9107e8b0a28647efd670f9e16
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2021-06-02 18:15:37 +1000

    Refactor

--- ps5/code/q2_neural_network_numpy.py
+++ ps5/code/q2_neural_network_numpy.py
@@ -1,110 +1,8 @@
 import numpy as np
-from scipy.optimize import check_grad
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 from plot_utils import save_plot, set_plot_style, MARKER_EDGE_WIDTH, MARKER_SIZE
-
-
-def f(p, x, y, N):
-    w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13 = p
-
-    # Hidden layer.
-    hidden_layer_inputs = np.hstack([
-        np.ones((N, 1)),
-        x
-    ])
-
-    hidden_layer_weights = np.array([
-        [w1,  w2,  w3],
-        [w4,  w5,  w6],
-        [w7,  w8,  w9]
-    ])
-
-    alpha_h = hidden_layer_inputs @ hidden_layer_weights
-    beta_h = sigmoid(alpha_h)
-
-    # Output layer.
-    output_layer_inputs = np.hstack([
-        np.ones((N, 1)),
-        beta_h
-    ])
-
-    output_layer_weights = np.array([
-        [w10, w11, w12, w13]
-    ]).T
-
-    alpha_o = output_layer_inputs @ output_layer_weights
-    beta_o = alpha_o  # No activation function on output neuron
-    y_pred = beta_o
-
-    # Calculate our loss function: the average error in our predictions
-    # compared to the target.
-    # (This is also known as the mean squared error).
-    return 0.5 * np.sum((y_pred - y)**2)
-
-
-def g(p, x, y, N):
-    w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13 = p
-
-    # Hidden layer.
-    hidden_layer_inputs = np.hstack([
-        np.ones((N, 1)),
-        x
-    ])
-
-    hidden_layer_weights = np.array([
-        [w1,  w2,  w3],
-        [w4,  w5,  w6],
-        [w7,  w8,  w9]
-    ])
-
-    alpha_h = hidden_layer_inputs @ hidden_layer_weights
-    beta_h = sigmoid(alpha_h)
-
-    # Output layer.
-    output_layer_inputs = np.hstack([
-        np.ones((N, 1)),
-        beta_h
-    ])
-
-    output_layer_weights = np.array([
-        [w10, w11, w12, w13]
-    ]).T
-
-    alpha_o = output_layer_inputs @ output_layer_weights
-
-    # Calculate gradients
-    s = (alpha_o - y)
-    dE_dw13 = s * beta_h[:, [2]]
-    dE_dw12 = s * beta_h[:, [1]]
-    dE_dw11 = s * beta_h[:, [0]]
-    dE_dw10 = s
-    dE_dw9 = s * w13 * beta_h[:, [2]] * (1 - beta_h[:, [2]]) * x[:, [1]]
-    dE_dw8 = s * w12 * beta_h[:, [1]] * (1 - beta_h[:, [1]]) * x[:, [1]]
-    dE_dw7 = s * w11 * beta_h[:, [0]] * (1 - beta_h[:, [0]]) * x[:, [1]]
-    dE_dw6 = s * w13 * beta_h[:, [2]] * (1 - beta_h[:, [2]]) * x[:, [0]]
-    dE_dw5 = s * w12 * beta_h[:, [1]] * (1 - beta_h[:, [1]]) * x[:, [0]]
-    dE_dw4 = s * w11 * beta_h[:, [0]] * (1 - beta_h[:, [0]]) * x[:, [0]]
-    dE_dw3 = s * w13 * beta_h[:, [2]] * (1 - beta_h[:, [2]])
-    dE_dw2 = s * w12 * beta_h[:, [1]] * (1 - beta_h[:, [1]])
-    dE_dw1 = s * w11 * beta_h[:, [0]] * (1 - beta_h[:, [0]])
-
-    return np.array([
-        np.sum(dE_dw1),
-        np.sum(dE_dw2),
-        np.sum(dE_dw3),
-        np.sum(dE_dw4),
-        np.sum(dE_dw5),
-        np.sum(dE_dw6),
-        np.sum(dE_dw7),
-        np.sum(dE_dw8),
-        np.sum(dE_dw9),
-        np.sum(dE_dw10),
-        np.sum(dE_dw11),
-        np.sum(dE_dw12),
-        np.sum(dE_dw13)
-    ])
 
 
 def read_data():
@@ -123,22 +21,28 @@ def normalize(x):
 
 
 def sigmoid(x):
+    """
+    Returns the value of the sigmoid function.
+    """
+
     return 1 / (1 + np.exp(-x))
 
 
-def run_model(N, hidden_layer_inputs, hidden_layer_weights, output_layer_weights):
+def run_model(n_observations, hidden_layer_inputs, hidden_layer_weights, \
+              output_layer_weights):
+
     hidden_layer_sums = hidden_layer_inputs @ hidden_layer_weights
     hidden_layer_outputs = sigmoid(hidden_layer_sums)
 
     output_layer_inputs = np.hstack([
-        np.ones((N, 1)),
+        np.ones((n_observations, 1)),
         hidden_layer_outputs
     ])
 
     return output_layer_inputs @ output_layer_weights, hidden_layer_outputs
 
 
-def calculate_gradients(x, y, N, y_pred, hidden_layer_outputs,
+def calculate_gradients(x, y, y_pred, hidden_layer_outputs,
                         output_layer_weights, gradients):
     w11 = output_layer_weights[1, 0]
     w12 = output_layer_weights[2, 0]
@@ -162,42 +66,53 @@ def calculate_gradients(x, y, N, y_pred, hidden_layer_outputs,
 
 
 def update_weights(gradients, eta, hidden_layer_weights, output_layer_weights):
-    hidden_layer_weights[0, 0] -= eta * gradients[0]  # w1
-    hidden_layer_weights[0, 1] -= eta * gradients[1]  # w2
-    hidden_layer_weights[0, 2] -= eta * gradients[2]  # w3
-    hidden_layer_weights[1, 0] -= eta * gradients[3]  # w4
-    hidden_layer_weights[1, 1] -= eta * gradients[4]  # w5
-    hidden_layer_weights[1, 2] -= eta * gradients[5]  # w6
-    hidden_layer_weights[2, 0] -= eta * gradients[6]  # w7
-    hidden_layer_weights[2, 1] -= eta * gradients[7]  # w8
-    hidden_layer_weights[2, 2] -= eta * gradients[8]  # w9
-    output_layer_weights[0, 0] -= eta * gradients[9]  # w10
-    output_layer_weights[1, 0] -= eta * gradients[10] # w11
-    output_layer_weights[2, 0] -= eta * gradients[11] # w12
-    output_layer_weights[3, 0] -= eta * gradients[12] # w13
+    hidden_layer_weights[0, 0] -= eta * gradients[0]   # w1
+    hidden_layer_weights[0, 1] -= eta * gradients[1]   # w2
+    hidden_layer_weights[0, 2] -= eta * gradients[2]   # w3
+    hidden_layer_weights[1, 0] -= eta * gradients[3]   # w4
+    hidden_layer_weights[1, 1] -= eta * gradients[4]   # w5
+    hidden_layer_weights[1, 2] -= eta * gradients[5]   # w6
+    hidden_layer_weights[2, 0] -= eta * gradients[6]   # w7
+    hidden_layer_weights[2, 1] -= eta * gradients[7]   # w8
+    hidden_layer_weights[2, 2] -= eta * gradients[8]   # w9
+    output_layer_weights[0, 0] -= eta * gradients[9]   # w10
+    output_layer_weights[1, 0] -= eta * gradients[10]  # w11
+    output_layer_weights[2, 0] -= eta * gradients[11]  # w12
+    output_layer_weights[3, 0] -= eta * gradients[12]  # w13
 
 
 def loss_function(y, y_pred):
     return 0.5 * np.sum((y_pred - y)**2)
 
 
-def make_input(x, n_inputs):
+def make_input(x):
+    """
+    Given the observations `x`, appends the biases and returns
+    [
+        [x1, x2, 1],
+        [x1, x2, 1],
+        [x1, x2, 1],
+        [x1, x2, 1]
+        ...
+        ...
+    ]
+    """
+    n_observations = x.shape[0]
+
     return np.hstack([
-        np.ones((n_inputs, 1)),
+        np.ones((n_observations, 1)),
         x
     ])
 
 
-def generate_weights(n_inputs, n_hidden):
-    weights = np.random.randn((n_inputs + 1) * n_hidden)
-
+def reshape_weights(hidden_weights, output_weights, n_inputs, n_hidden):
     # Make the following inner weights for the inner layer:
     # [
     #     [w1,  w2,  w3],
     #     [w4,  w5,  w6],
     #     [w7,  w8,  w9]
     # ]
-    hidden_layer_weights = weights.reshape(n_inputs + 1, n_hidden)
+    hidden_layer_weights = hidden_weights.reshape(n_inputs + 1, n_hidden)
 
     # Outer layer weights:
     # [
@@ -206,42 +121,29 @@ def generate_weights(n_inputs, n_hidden):
     #     [w12],
     #     [w13]
     # ]
-    output_layer_weights = np.random.randn(n_hidden + 1).reshape(n_hidden + 1, 1)
+    output_layer_weights = output_weights.reshape(n_hidden + 1, 1)
+
     return hidden_layer_weights, output_layer_weights
 
 
-def run():
-    """An entry point of the script"""
+def generate_weights(n_inputs, n_hidden):
+    hidden_weights = np.random.randn((n_inputs + 1) * n_hidden)
+    output_weights = np.random.randn(n_hidden + 1)
+    return reshape_weights(hidden_weights, output_weights, n_inputs, n_hidden)
 
-    np.random.seed(0)
-    X, y = read_data()
-    x = normalize(X)
-    N = x.shape[0]  # Number of observations
-    n_inputs = x.shape[1]
-    n_hidden = 3  # The number of neurons in the hidden layer.
-    assert check_grad(f, g, np.random.normal(size=13), x, y, N) < 1e-4
-    hidden_layer_inputs = make_input(x, N)
-    hidden_layer_weights, output_layer_weights = generate_weights(n_inputs, n_hidden)
 
-    y_pred, _ = run_model(
-        N, hidden_layer_inputs,
-        hidden_layer_weights, output_layer_weights)
+def train_model(x, y, num_epochs, n_observations, hidden_layer_inputs,
+                hidden_layer_weights, output_layer_weights):
 
-    print(f"Initial loss: {loss_function(y, y_pred):.0e}")
-
-    # Back propagation
-    # ---------
-
-    num_epochs = 1000
     losses = np.empty(num_epochs)
     gradients = np.empty(13)
 
     for epoch in range(num_epochs):
         y_pred, hidden_layer_outputs = run_model(
-            N, hidden_layer_inputs,
+            n_observations, hidden_layer_inputs,
             hidden_layer_weights, output_layer_weights)
 
-        calculate_gradients(x, y, N, y_pred, hidden_layer_outputs,
+        calculate_gradients(x, y, y_pred, hidden_layer_outputs,
                             output_layer_weights, gradients)
 
         update_weights(gradients, 1e-3, hidden_layer_weights,
@@ -255,9 +157,10 @@ def run():
 
         losses[epoch] = loss
 
-    # Plot loss reducing with time
-    # ---------
+    return losses
 
+
+def plot_losses(losses):
     fig, ax = plt.subplots(figsize=(6, 6))
     ax.plot(losses, zorder=2, color='#ff0021')
     ax.set_xlabel('Epoch')
@@ -270,6 +173,37 @@ def run():
     save_plot(plt, suffix='01')
 
 
+def initialize_and_train_model(X, y):
+    x = normalize(X)
+    n_observations = x.shape[0]  # Number of observations
+    n_inputs = x.shape[1]
+    n_hidden = 3  # The number of neurons in the hidden layer.
+    hidden_layer_inputs = make_input(x)
+
+    hidden_layer_weights, output_layer_weights = generate_weights(
+        n_inputs, n_hidden
+    )
+
+    losses = train_model(
+        x=x, y=y, num_epochs=1000, n_observations=n_observations,
+        hidden_layer_inputs=hidden_layer_inputs,
+        hidden_layer_weights=hidden_layer_weights,
+        output_layer_weights=output_layer_weights
+    )
+
+    return hidden_layer_weights, output_layer_weights, losses
+
+
+def entry_point():
+    np.random.seed(0)
+    x, y = read_data()
+
+    hidden_layer_weights, output_layer_weights, losses = \
+        initialize_and_train_model(x, y)
+
+    plot_losses(losses)
+
+
 if __name__ == "__main__":
-    run()
+    entry_point()
     print('We are done')
---
+++ ps5/code/q2_neural_network_numpy_test.py
@@ -0,0 +1,76 @@
+from scipy.optimize import check_grad
+import numpy as np
+
+from q2_neural_network_numpy import sigmoid, make_input, reshape_weights, \
+     run_model, loss_function, calculate_gradients
+
+from pytest import approx
+
+
+def test_sigmoid():
+    assert sigmoid(2) == approx(0.880797, rel=1e-4)
+
+
+def function(p, x, y, n_inputs, n_hidden):
+    hidden_layer_inputs = make_input(x)
+    n_observations = x.shape[0]
+    n_hidden_weights = (n_inputs + 1) * n_hidden
+    hidden_weights = p[0: n_hidden_weights]
+    output_weights = p[n_hidden_weights:]
+
+    hidden_layer_weights, output_layer_weights = reshape_weights(
+        hidden_weights, output_weights, n_inputs, n_hidden)
+
+    y_pred, _ = run_model(
+        n_observations, hidden_layer_inputs,
+        hidden_layer_weights, output_layer_weights)
+
+    return loss_function(y, y_pred)
+
+
+def gradient(p, x, y, n_inputs, n_hidden):
+    hidden_layer_inputs = make_input(x)
+    n_observations = x.shape[0]
+    n_hidden_weights = (n_inputs + 1) * n_hidden
+    hidden_weights = p[0: n_hidden_weights]
+    output_weights = p[n_hidden_weights:]
+
+    hidden_layer_weights, output_layer_weights = reshape_weights(
+        hidden_weights, output_weights, n_inputs, n_hidden)
+
+    y_pred, hidden_layer_outputs = run_model(
+            n_observations, hidden_layer_inputs,
+            hidden_layer_weights, output_layer_weights)
+
+    gradients = np.empty(len(p))
+
+    calculate_gradients(x, y, y_pred, hidden_layer_outputs,
+                        output_layer_weights, gradients)
+
+    return gradients
+
+
+def test_test_grads():
+    n_inputs = 2
+    n_hidden = 3
+
+    x = np.array(
+        [
+            [1, 2],
+            [4, 5],
+            [-2, 9]
+        ])
+
+    y = [
+            [1],
+            [2],
+            [12]
+        ]
+
+    x0 = np.random.normal(size=13)
+
+    grad_diff = check_grad(
+        function, gradient, x0, x, y, n_inputs, n_hidden
+    )
+
+    assert grad_diff < 1e-4
